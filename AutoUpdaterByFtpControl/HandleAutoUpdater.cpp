490b091d765d55db42fc78b36d4be998d94474a6
821181867@qq.com <821181867@qq.com>
2019-12-30 15:52:34 +0800

    用来控制更新主程序启动并解压覆盖更新文件

---
+++ AutoUpdaterByFtpControl/HandleAutoUpdater.cpp
@@ -0,0 +1,86 @@
+﻿#include "HandleAutoUpdater.h"
+#include <QApplication>
+#include <QDebug>
+#include <QSettings>
+#include <Windows.h>
+#include <QTimer>
+#include <QFileInfo>
+#include <QDir>
+
+#define HANDLEAUTOUPDATER_EXENAME "AutoUpdaterByFtp.exe"
+#define HANDLEAUTOUPDATER_UPDATEWORKDIR "/updateDir"
+#define HANDLEAUTOUPDATER_UPDATEINFOFILE "/updateInfo.ini"
+#define HANDLEAUTOUPDATER_KEY_VERSION "updateInfo/Version"
+#define HANDLEAUTOUPDATER_KEY_UPDATEFILE "updateInfo/updateFile"
+
+HandleAutoUpdater::HandleAutoUpdater(QObject *parent) : QObject(parent)
+{
+
+}
+
+void HandleAutoUpdater::startHandle()
+{
+    if(!checkHaveUpdateModle())
+    {
+        return;
+    }
+    //
+    QString updatefile = checkIsUpdateVersion();
+    if(updatefile.isEmpty())
+    {
+        //版本未更新开始检测
+        startCheckUpdateVersion();
+    }
+    else
+    {
+        //版本更新,启动解压更新
+        startZipUpdateVersion();
+    }
+}
+
+QString HandleAutoUpdater::checkIsUpdateVersion()
+{
+    QString updateDir = QApplication::applicationDirPath()+HANDLEAUTOUPDATER_UPDATEWORKDIR;
+    QDir dir(updateDir);
+    if(!dir.exists())
+    {
+        dir.mkdir(updateDir);
+    }
+    QString updateInfofile = updateDir+HANDLEAUTOUPDATER_UPDATEINFOFILE;
+    QSettings settings(updateInfofile, QSettings::IniFormat);
+    QString downVersion = settings.value(HANDLEAUTOUPDATER_KEY_VERSION,QApplication::applicationVersion()).toString();
+    if(QApplication::applicationVersion()==downVersion)
+    {
+        //版本未更新
+        return QString();
+    }
+    return settings.value(HANDLEAUTOUPDATER_KEY_UPDATEFILE,"").toString();
+}
+
+void HandleAutoUpdater::startCheckUpdateVersion()
+{
+    m_pProcess = new QProcess(this);
+    QString checkUpdateExe = QApplication::applicationDirPath()+"/"+HANDLEAUTOUPDATER_EXENAME;
+    QString appName = QApplication::applicationName();
+    QString appVersion = QApplication::applicationVersion();
+    QString appPid = QString::number(GetCurrentProcessId());
+    m_pProcess->start(checkUpdateExe,
+                      QStringList()<<appName<<appVersion<<appPid);
+    qDebug()<<__FUNCTION__<<checkUpdateExe<<appName<<appVersion<<appPid;
+}
+
+void HandleAutoUpdater::startZipUpdateVersion()
+{
+    QFileInfo fileinfo(QApplication::applicationFilePath());
+    QString qstrFileName = QApplication::applicationDirPath()+"/"+HANDLEAUTOUPDATER_EXENAME;
+    qstrFileName=qstrFileName.replace('/',"\\");
+    qDebug()<<__FUNCTION__<<qstrFileName<<fileinfo.fileName()<<ShellExecuteA(NULL, "runas", qstrFileName.toStdString().c_str() , fileinfo.fileName().toStdString().c_str(), NULL, SW_SHOWNORMAL);
+    QTimer::singleShot(0,[]{
+        QApplication::exit();
+    });
+}
+
+bool HandleAutoUpdater::checkHaveUpdateModle()
+{
+    return QFile::exists(QApplication::applicationDirPath()+"/"+HANDLEAUTOUPDATER_EXENAME);
+}
---
+++ AutoUpdaterByFtpControl/HandleAutoUpdater.h
@@ -0,0 +1,29 @@
+﻿#ifndef HANDLEAUTOUPDATER_H
+#define HANDLEAUTOUPDATER_H
+
+#include <QObject>
+#include <QProcess>
+
+class HandleAutoUpdater : public QObject
+{
+    Q_OBJECT
+public:
+    explicit HandleAutoUpdater(QObject *parent = nullptr);
+    //
+    void startHandle();
+
+signals:
+
+public slots:
+
+private:
+    bool checkHaveUpdateModle();
+    //
+    QString checkIsUpdateVersion();
+    void startCheckUpdateVersion();
+    void startZipUpdateVersion();
+private:
+    QProcess *m_pProcess;
+};
+
+#endif // HANDLEAUTOUPDATER_H
